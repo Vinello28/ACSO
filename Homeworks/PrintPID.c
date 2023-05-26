9c285b0f33e8affd089d8eadc7071fb1ed870a6e
Vinello28 <74827522+Vinello28@users.noreply.github.com>
2023-05-26 16:17:42 +0200

    e1

--- Homeworks/PrintPID.c
+++ Homeworks/PrintPID.c
@@ -4,10 +4,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
  int main (){
      printf("---- PRINT PID ----\n");
-
-     fork()
+     int pidP = getpid();
+     int pidS = fork();
+     printf("PID father: %d\n PID son: %d", pidP, pidS);
+     if(pidS == -1)printf("ERROR: in creating another process");
+     wait(&pidS);
+     return 0;
  }
